f7586122a16e458b27149ad7abb63a0d4b2237af
Nicke van Oorschot <nickevanoorschot@gmail.com>
2020-01-09 22:28:08 +0100

    Ignore any double qoutes when sorting the columns

--- lib/activerecord-clean-db-structure/clean_dump.rb
+++ lib/activerecord-clean-db-structure/clean_dump.rb
@@ -182,13 +182,19 @@ module ActiveRecordCleanDbStructure
     end
     
     # Orders the columns definitions alphabetically
+    # - ignores quotes which surround column names that are equal to reserved PostgreSQL names.
     # - keeps the columns at the top and places the constraints at the bottom.
     def order_column_definitions
       dump.gsub!(/^(?<table>CREATE TABLE .+?\(\n)(?<columns>.+?)(?=\n\);$)/m) do
-        [
-          $~[:table],
-          $~[:columns].split(",\n").sort.partition { |column| !column.match?(/\A *CONSTRAINT/) }.flatten.join(",\n")
-        ].join
+        columns =
+          $~[:columns]
+          .split(",\n")
+          .sort_by { |column| column[/[^ "]+/] }
+          .partition { |column| !column.match?(/\A *CONSTRAINT/) }
+          .flatten
+          .join(",\n")
+
+        [$~[:table], columns].join
       end
     end
 
